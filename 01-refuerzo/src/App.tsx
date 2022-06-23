5229f6602c8a50f7d828d0212f436d9e9ebe9570
unknown <vidaguilar22102001@gmailcom>
2022-06-23 11:00:02 -0500

    Agregando proyecto

--- 01-refuerzo/src/App.tsx
+++ 01-refuerzo/src/App.tsx
@@ -1,3 +1,4 @@
+import TiposBasicos from "./typescript/001-TiposBasicos"
 
 
 
@@ -5,6 +6,9 @@ const App = () =>{
 return(
   <div className="mt-2">
   <h1>Inicio Con Typescript</h1>
+ <hr />
+  
+  <TiposBasicos />
   </div>
   
 )
